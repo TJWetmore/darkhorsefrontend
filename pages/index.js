b7b849c2f07c51bd9e57d24762244b2e6a0458c2
TJ Wetmore <Thomas.J.Wetmore@gmail.com>
2021-03-07 12:00:20 -0500

    updated firebase rules

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,16 @@ export default function Home() {
         <title>Dark Horse Fantasy Sports</title>
         <link rel="icon" href="/DarkHorse.ico" />
       </Head>
+
+        {/* <!-- The core Firebase JS SDK is always required and must be listed first --> */}
+        <script src="/__/firebase/8.2.10/firebase-app.js"></script>
+
+        {/* <!-- TODO: Add SDKs for Firebase products that you want to use
+            https://firebase.google.com/docs/web/setup#available-libraries --> */}
+        <script src="/__/firebase/8.2.10/firebase-analytics.js"></script>
+
+        {/* <!-- Initialize Firebase --> */}
+        <script src="/__/firebase/init.js"></script>
       {user !== null? 
       <DashBoard />
       :
